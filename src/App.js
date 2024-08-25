c3b63702612bb557696c9af14f20d7f0c0674d59
Emmanuel Pius <emmycode4495@gmail.com>
2024-08-25 10:03:00 +0100

    Completed all required pages

--- src/App.js
+++ src/App.js
@@ -3,11 +3,21 @@ import './App.css';
 
 import 'bootstrap/dist/css/bootstrap.min.css'
 import NavBarPage from './components/NavBarPage';
+import BannerPage from './components/BannerPage';
+import SkillsPage from './components/SkillsPage';
+import Projects from './components/Projects';
+import Contact from './components/Contacts';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
       <NavBarPage/>
+      <BannerPage/>
+      <SkillsPage/>
+      <Projects/>
+      <Contact/>
+      <Footer/>
     </div>
   );
 }
